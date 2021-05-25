2c20e7eb01ca705f003d810b765f522c58b705d3
Waccsadac <roeypeleg@gmail.com>
2021-05-25 18:50:30 +0300

    Imported smallestEnclosingCircle, added simple (linear) algorithm for learn and detect

--- Model/Detector.js
+++ Model/Detector.js
@@ -1,18 +1,19 @@
-const cpp = 5 //// TODO : make this connection to cpp api.
-    function algorithm_Setting(type) {
-        cpp.algorithm_Setting(type);
-    }
-    function learn(learnFile) {
-        cpp.learnNormal(learnFile);
-    }
-    function detect(detectFile){
-        return cpp.detect(detectFile);
-    }
-    function open(){
-        cpp.openConnection();
-    }
-    function close(){
-        cpp.closeConnection();
-    }
+const ts = require('TimeSeries');
+function algorithm_Setting(type) {
+    ts.algorithm_setting = type;
+}
+function learn(learnFile) {
+    ts.createTimeSeries(learnFile)
+    ts.learn();
+}
+function detect(detectFile) {
+    return ts.detect(detectFile);
+}
+function open(){
+    
+}
+function close(){
+    
+}
 
 module.exports = {algorithm_Setting, learn, detect, open, close};
--- Model/TimeSeries.js
+++ Model/TimeSeries.js
@@ -40,3 +40,175 @@ class TimeSeries {
 		}
 	}
 }
+
+class correlatedFeatures {
+	constructor(feat1, feat2, correlation, lin_reg, threshold, center) {
+		this.feat1 = feat1;
+		this.feat2 = feat2;
+		this.correlation = correlation;
+		this.lin_reg = lin_reg;
+		this.threshold = threshold;
+		this.center = center;
+	}
+}
+
+class Line {
+	constructor(a, b) {
+		this.a = a;
+		this.b = b;
+		this.f = function (x) {
+			return a * x + b;
+		}
+	}
+}
+
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.setCords = function (x, y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
+
+class AnomalyReport {
+	constructor(description, timeStep, val1, val2) {
+		this.description = description;
+		this.timeStep = timeStep;
+		this.val1 = val1;
+		this.val2 = val2;
+	}
+}
+var cf = [];
+var ts;
+var chosenAlgorithm;
+createTimeSeries = function (file) {
+	ts = new TimeSeries(file)
+}
+
+algorithm_setting = function (type) {
+	chosenAlgorithm = type;
+}
+
+function AVG(c) {
+	return c.reduce((a, b) => (a + b)) / c.length;
+}
+
+function VAR(c) {
+	const avg = AVG(c);
+	var variance = 0;
+	for (var i = 0; i < c.length; i++) {
+		variance += (c[i] - avg) * (c[i] - avg);
+	}
+	variance /= c.length;
+	return variance
+}
+
+function COAVG(c1, c2) {
+	var coavg = 0;
+	for (var i = 0; i < c1.length; i++) {
+		coavg += c1[i] * c2[i];
+	}
+	return coavg / c1.length;
+}
+
+function COV(c1, c2) {
+	const cov = COAVG(c1, c2) - AVG(c1) * AVG(c2)
+	return cov;
+}
+
+function Pearson(c1, c2) {
+	const p = COV(c1, c2) / (Math.sqrt(VAR(c1)) * Math.sqrt(VAR(c2)));
+	return p;
+}
+
+function LinReg(c1, c2) {
+	const a = COV(c1, c2) / VAR(c1);
+	const b = AVG(c2) - a * AVG(c1);
+	return new Line(a, b);
+}
+
+function getMaxDev(c1, c2, linreg) {
+	var maxDev = 0;
+	for (var i = 0; i < c1.length; i++) {
+		var currDev = dev(new Point(c1[i], c2[i]), linreg)
+		if (currDev > maxDev) {
+			maxDev = currDev;
+		}
+	}
+	return maxDev;
+}
+
+function dev(p, l) {
+	x = p.y - l.f(p.x);
+	return Math.abs(x);
+}
+
+function addCorrelation(ts, col_i1, col_i2, max) {
+	if (max > 0.9) {
+		const f1 = ts.GetColumnNames[col_i1];
+		const f2 = ts.GetColumnNames[col_i2];
+		const correlation = max;
+		const c1 = ts.GetColumnByIndex[col_i1];
+		const c2 = ts.GetColumnByIndex[col_i2];
+		const lin_reg = LinReg(c1, c2);
+		const thresh = getMaxDev(c1, c2, lin_reg);
+		var corFeat = new correlatedFeatures(f1, f2, correlation, lin_reg, thresh, center);
+		cf.push(corFeat);
+	}
+}
+
+function isAnomaly(ts, cf, timeStep) {
+	if (chosenAlgorithm = "simple") {
+		var Column1 = ts.GetColumn(cf.feature1);
+		var Column2 = ts.GetColumn(cf.feature2);
+		currPoint = new Point(Column1[timeStep], Column2[timeStep]);
+		currDev = dev(currPoint, cf.lin_reg);
+		return (currDev > cf.threshold * 1.1);
+	}
+}
+
+learn = function () {
+	cf = []
+	if (chosenAlgorithm == "simple") {
+		for (var i = 0; i < ts.NumOfColumns() - 1; i++) {
+			var Col1 = ts.GetColumnByIndex(i);
+			var max = 0;
+			var maxIndex = -1;
+			for (var j = i + 1; j < ts.NumOfColumns(); j++) {
+				var Col2 = ts.GetColumnByIndex(j);
+				const correlativity = Math.abs(Pearson(Col1, Col2));
+				if (correlativity > max) {
+					max = correlativity;
+					maxIndex = j;
+				}
+			}
+			if (maxIndex >= 0) {
+				addCorrelation(ts, i, j, max)
+			}
+		}
+	}
+}
+
+getNormalModel = function () {
+	return cf;
+}
+
+detect = function (file) {
+	ts = new TimeSeries(file);
+	var anomalies = [];
+	const numOfRows = ts.NumOfRows;
+	const cflen = cf.length;
+	for (var cfIndex = 0; cfIndex < cflen; cfIndex++) {
+		for (var timeStep = 0; timeStep < numOfRows; timeStep++) {
+			if (isAnomaly(ts, cf[cfIndex], timeStep)) {
+				var report = new AnomalyReport(cf[cfIndex].feat1 + "-" + cf[cfIndex].feat2, timeStep, ts.GetColumn(cf[cfIndex].feat1)[timeStep], ts.GetColumn(cf[cfIndex].feat2)[timeStep]);
+				anomalies.push(report);
+			}
+		}
+	}
+	return anomalies;
+}
+module.exports = { algorithm_setting, createTimeSeries, learn, detect };
