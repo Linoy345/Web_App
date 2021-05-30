11c26b36f444bdfcfb0fa491498edb2311480211
Linoy345 <72787752+Linoy345@users.noreply.github.com>
2021-05-30 11:55:30 +0300

    update with table anomalies

--- Controller/Server.js
+++ Controller/Server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const detector = require('../Model/Detector');
+const table = require('../Controller/buildTable');
+
 // const path = require('path');
 // const index = path.resolve('../View/index.html');
 // const view = path.resolve('../View');
@@ -25,6 +27,7 @@ app.post('/detect', (req, res) => {
         detector.learn(req.files.learn_file.data.toString());
         detector.algorithm_Setting(req.body.setting.toString());
         let anomalies = detector.detect(req.files.detect_file.data.toString());
+        table.createTable(anomalies);
         res.write(JSON.stringify(anomalies));
     }
     res.end();
